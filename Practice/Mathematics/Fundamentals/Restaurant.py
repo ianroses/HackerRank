efd789596201fcdd313b9e13da00cfb3bd15f7fc
Ian Rosas <34254449+ianroses@users.noreply.github.com>
2019-01-22 08:59:09 -0600

    Create Restaurant.py

---
+++ Practice/Mathematics/Fundamentals/Restaurant.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the restaurant function below.
+#
+def restaurant(l, b):
+    x = l
+    y = b
+    if x > y: 
+        small = y
+    else: 
+        small = x 
+    for i in range(1, small+1): 
+        if((x % i == 0) and (y % i == 0)): 
+            gcd = i 
+    return int((l/gcd)*(b/gcd))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        lb = input().split()
+
+        l = int(lb[0])
+
+        b = int(lb[1])
+
+        result = restaurant(l, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
