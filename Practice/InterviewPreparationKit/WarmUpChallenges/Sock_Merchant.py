1fa6e1e9ca603b615a07b8e3e26ae643ebc048b6
Ian Rosas <34254449+ianroses@users.noreply.github.com>
2019-06-27 11:37:29 -0500

    Create Sock_Merchant.py

---
+++ Practice/InterviewPreparationKit/WarmUpChallenges/Sock_Merchant.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    ar.sort()
+    pairs = 0
+    while len(ar)>0:
+        num = ar.count(ar[0])
+        pairs = pairs + num//2
+        ar = ar[num:]
+    return pairs
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
